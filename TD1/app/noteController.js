2412cfa7481255d81b4212c4250d757eec70c9cc
MLaunay <21500607@etu.unicaen.fr>
2017-01-30 19:59:09 +0100

    Message + classe

--- TD1/app/noteController.js
+++ TD1/app/noteController.js
@@ -1,14 +1,33 @@
-angular.module("NoteApp").controller("NoteController",function(){
-	this.messageNote = "";
-	this.info = ["modifiée","sauvegardée"];
+angular.module("NoteApp").controller("NoteController",['$cookies', function($cookies){
+	
+	this.info = "";
 	this.status = 1;
-	this.save = function(){
-		//$cookies.put('message',messageNote);
-	};
+	this.saved = $cookies.get("message");
+	if(this.saved){
+		this.messageNote = this.saved;
+	}
+	else {
+		this.messageNote = "";
+	}
+	
 	this.clear = function(){
 		this.messageNote = "";
 	};
 	this.count = function(){
-		return 100 - this.messageNote.length;
-	};	
-});
+		
+		return 100 - this.messageNote.length;;
+	};
+	this.save = function(){
+		if (this.count() >= 0){
+            $cookies.put('message', this.messageNote);
+            this.info = "Note sauvegardée";
+            
+        }
+		
+	};
+	this.modifie = function(){
+		if(this.messageNote != this.saved){
+			this.info = "Note modifiée";
+		}
+	};
+}]);

b7614c491ffcfe8c8b872e420474538e94e845b6
Mathis LAUNAY <Mathis LAUNAY>
2017-03-10 11:01:50 +0100

    Correction historique

--- TD3/app/ConvertisseurService.js
+++ TD3/app/ConvertisseurService.js
@@ -7,31 +7,30 @@ angular.module("ConvertisseurApp").service("ConvertisseurService", function(){
 	self.update = function(from,to,what,hasHisto,result){
 		
 		var tx = result/what;
-		
 		var conversion={from : from, 
         to : to,
 		rate : tx, 
-        amount : function(){ return this.rate*this.what; }, 
-        initialAmount : function(){ return this.initialRate*amount();},
+        amount : function(){ return this.rate*what; }, 
+        initialAmount : function(){ return this.initialRate*what;},
         delta : function() {return this.rate-this.initialRate;},
         what : what, 
 		date : new Date().toLocaleString(),
         update: 1, 
         initialRate : tx
         };
-		console.log("delta = "+conversion.delta);
+		console.log("amount = "+conversion.amount());
+
 		var key = self.from.code+'_'+self.to.code;
 		
 		if(self.historique[key]){
-			console.log("J'existe déjà");
+			//console.log("J'existe déjà");
 			self.historique[key].date = new Date().toLocaleString();
 			self.historique[key].delta = conversion.delta;
-			self.historique[key].rate = tx;
 		}
 		else {
-			console.log("Je n'existe pas déjà");
+			//console.log("Je n'existe pas déjà");
 			self.historique[key] = conversion;
-			console.table(self.historique);
+			//console.table(self.historique);
 		}
 		
 	};
